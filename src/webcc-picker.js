d13e3df0172185da30d18781863cb27a97463630
Arnaud NICOLAS <arno06@gmail.com>
2022-10-31 23:19:45 +0100

    DatePicker

--- src/webcc-picker.js
+++ src/webcc-picker.js
@@ -1,3 +1,39 @@
-class WebccPicker{
+const WebCCPicker = (function(){
+    function register(pElement){
+        let calendar = document.createElement('webc-calendar');
+        calendar.setAttribute("selected-dates", pElement.getAttribute("value"));
+        calendar.style.width = "350px";
+        calendar.style.position = "absolute";
+        calendar.style.top = "100%";
+        calendar.style.left = "0";
+        calendar.style.marginTop = "10px";
+        calendar.style.display = "none";
+        pElement.parentNode.style.position = "relative";
+        pElement.parentNode.appendChild(calendar);
 
-}
+        pElement.addEventListener('focus', function(){
+            calendar.setAttribute("selected-dates", "");
+            calendar.setAttribute("selected-dates", pElement.getAttribute("value"));
+            calendar.style.display = "block";
+        });
+
+        document.addEventListener('click', function(e){
+            if(e.target === pElement || e.target === calendar){
+                return;
+            }
+            calendar.style.display = "none";
+        });
+
+        calendar.addEventListener(WebCCalendar.EVENT_DAY_SELECTED, function(e){
+            pElement.setAttribute("value", e.detail.value);
+            calendar.style.display = "none";
+        });
+    }
+    function init(){
+        document.querySelectorAll('input[data-comp="webc-calendar"]').forEach(register);
+    }
+    document.addEventListener('DOMContentLoaded', init);
+    return {
+        'register':register
+    };
+})();
